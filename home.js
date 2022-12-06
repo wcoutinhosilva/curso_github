cc68a800ecc299bbf0861d70d8f9fb9030e5ef3d
Welisson <wcoutinhosilva@hotmail.com>
2022-12-06 20:46:17 -0300

    Nova home finalizada

--- home.js
+++ home.js
@@ -1,3 +1,9 @@
 function mostraNome(nome){
     return nome
 }
+
+function mostraSobreNome(sobreNome){
+    return sobreMome;
+}
+
+//Finalizado o sistema da home
--- login.js
+++ login.js
@@ -2,3 +2,11 @@
 //trabalhando no login
 
 //finalizando sistema de login
+
+
+function login (nome){
+    return nome;
+}
+
+//Sistema de login completo
+//Retornomos o nome do usuario quando logado
